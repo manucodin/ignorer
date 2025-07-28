bf6211d569748b19d40f5d5d0ab8c575c98be1d3
GitHub Action <action@github.com>
2025-07-28 20:25:12 +0000

    Update Homebrew formula to v0.1.5

--- Formula/ignorer.rb
+++ Formula/ignorer.rb
@@ -1,8 +1,8 @@
 class Ignorer < Formula
   desc "Smart .gitignore generator - Generate .gitignore files from predefined templates"
   homepage "https://github.com/manucodin/ignorer"
-  url "https://github.com/manucodin/ignorer/archive/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://github.com/manucodin/ignorer/archive/v0.1.5.tar.gz"
+  sha256 "74f94af4fcb7d3c62ec5cd0e5cf151ab17560341b1f000c16f444346b6ae2a8d"
   license "MIT"
   
   depends_on "go" => :build
